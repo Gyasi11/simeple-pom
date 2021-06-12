6119d24212931d65217f4f2fc5eee791ac8d9146
Gyasi11 <glcalhoun1123@yahoo.com>
2021-06-12 12:42:12 -0400

    Add Circular Progrsss Bar Functionality

--- my-simple-pom/src/App.js
+++ my-simple-pom/src/App.js
@@ -1,41 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import React, { useState, useEffect } from 'react'
+import Complete from './Complete';
 
 const PomodoroTimer = () => {
 
   // change variable names
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const [progress, setProgress] = React.useState(100);
+  let pomodoroTime = 120
 
-  function pomodoroToggle(){
+  function pomodoroToggle() {
     setIsActive(!isActive)
   }
 
-  function resetPomodoro(){
+  function resetPomodoro() {
     setSeconds(0)
+    setProgress(100)
     setIsActive(false)
   }
 
   useEffect(() => {
     let interval = null;
-    if(isActive) {
+    if (isActive) {
       interval = setInterval(() => {
         setSeconds(seconds => seconds + 1);
+        setProgress(progress => progress - 0.03703704);
       }, 1000);
-    } else if(!isActive && seconds !== 0) {
+
+      if (seconds > 2700) {
+       resetPomodoro()
+      }
+    } else if (!isActive && seconds !== 0) {
       clearInterval(interval)
     }
-
     return () => clearInterval(interval)
-  }, [isActive, seconds])
+  }, [isActive, seconds, progress])
 
   return (
-    <div>
-      <div>
-      {seconds}s
+    <div className="Container">
+      <div className="ProgressbarContainer">
+        <CircularProgress size={500} variant="determinate" value={progress} />
       </div>
       <div>
+
+        <h1 className="Time">{seconds}s</h1>
+      </div>
+      <div className="ButtonContainer">
         <button onClick={pomodoroToggle}>Start</button>
         <button onClick={resetPomodoro}>Reset</button>
       </div>
---
+++ my-simple-pom/src/Complete.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect } from 'react'
+
+const Complete = () => {
+  return (
+    <div className="Container">
+      <h1>Pomodoro Timer Complete</h1>
+    </div>
+  )
+}
+
+export default Complete;
